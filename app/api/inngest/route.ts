eef40d4c14075ebc6d68df951a4024bdf8755c79
Aman <patelaman0241@gmail.com>
2025-12-17 20:47:05 +0530

    Implement daily news summary

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,8 +1,8 @@
 import {serve} from "inngest/next";
 import { inngest } from "@/lib/inngst/client";
-import { sendSignUpEmail } from "@/lib/inngst/function";
+import {sendDailyNewsSummary, sendSignUpEmail} from "@/lib/inngst/function";
 
 export const { GET, POST, PUT } = serve({
     client: inngest,
-    functions: [sendSignUpEmail],
+    functions: [sendSignUpEmail, sendDailyNewsSummary],
 })
